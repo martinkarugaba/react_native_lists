3305ceebbdac105048f577ba7559789ed8c23ec8
Martin Karugaba <martinkarugaba21@gmail.com>
2024-03-02 13:26:03 +0300

    Render lists using map method

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  StatusBar,
+} from 'react-native';
+import mentorsList from './data.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {mentorsList.map((mentor) => {
+          console.log(mentor.id)
+          return (
+            <View style={styles.card} key={mentor.id}>
+              <Text style={styles.cardText}>{mentor.mentor}</Text>
+              <Text>{mentor.location}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
   },
+  scrollView: {
+    paddingHorizontal: 20,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginVertical: 15,
+  },
+  cardText: {
+    fontSize: 30,
+  }
 });
